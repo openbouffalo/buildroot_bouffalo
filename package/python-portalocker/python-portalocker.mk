57c6d47d3c393c8ef6430f7e8a802aeea94b14bc
Pavel Zakopaylo <zakopaylo2@gmail.com>
2023-11-20 15:49:19 +1100

    Dependency cleanup for bl_mcu_sdk
    * python-portalocker reset to v2.0.0 in line with requirements
      file in bflb-iot-tool
    
    * Removed auto-generated Config.in files; these can be replaced with
      correct Config.in.host files at a later date.

--- package/python-portalocker/python-portalocker.mk
+++ package/python-portalocker/python-portalocker.mk
@@ -1,11 +1,10 @@
 ################################################################################
 #
-# python-ecdsa
+# python-portalocker
 #
 ################################################################################
 
-PYTHON_PORTALOCKER_VERSION = master
-#PYTHON_ECDSA_SOURCE = ecdsa-$(PYTHON_ECDSA_VERSION).tar.gz
+PYTHON_PORTALOCKER_VERSION = v2.0.0
 PYTHON_PORTALOCKER_SITE = $(call github,wolph,portalocker,$(PYTHON_PORTALOCKER_VERSION))
 PYTHON_PORTALOCKER_SETUP_TYPE = setuptools
 PYTHON_PORTALOCKER_LICENSE = BSD-3-CLAUSE
